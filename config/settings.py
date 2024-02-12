e01ead1760c20d55fe03918c222f1ee93905adcc
Ваше имя <@.com>
2024-02-12 13:29:58 +0600

    channels

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('34.125.39.164', 6379)],
+            "hosts": [('redis', 6379)],
         },
     },
 }
